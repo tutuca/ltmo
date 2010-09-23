ba0a02cb8c826924b847cfa042e810afd336ad5d
Matias Iturburu <maturburu@gmail.com>
2010-09-23 00:37:07 -0300

    saqué la dependencia en el api de github porque era al pedo y nos mata

--- context_processors.py
+++ context_processors.py
@@ -2,14 +2,9 @@ import os
 
 from django.conf import settings
 from django.utils import simplejson as json
-from github2.client import Github
-
-
 
 def about(request):
-    github = Github(username=settings.GITHUB_USER, api_token=settings.GITHUB_API_TOKEN)
     f = open(os.path.join(settings.BASE_DIR, 'ABOUT'), 'r')
     return {
         'about':str(f.read()),
-        'repository':github.repos.show("tutuca/ltmo")
     }
