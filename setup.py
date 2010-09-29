62d47ba3340604cf9faf7dd9fc572b055748840b
Matias Iturburu <maturburu@gmail.com>
2010-09-28 21:33:19 -0300

    Creo que ya está bien para ser registrado en pypi. Necesitaría probar un par de casos de instalación, pero de cualqueir manera habría que registrarlo.
    Agrego la documentación de la biblioteca de macros de Michal Ludvig
    <michal@logix.cz> en el directorio docs

--- setup.py
+++ setup.py
@@ -5,9 +5,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='ltmo',
-    version='0.8dev',
+    version='0.11dev',
     author=u'Matías Iturburu, Francisco Herrero',
     author_email='maturburu@gmail.com, francisco.herrero@gmail.com',
     url='http://github.com/tutuca/ltmo',
     description = 'Un sitio para los amigos',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'PIL',
+        'markdown',
+        'psycopg2',
+        'django',
+        'django-tagging'
+        'django-pagination'
+    ]
+
+
 )
