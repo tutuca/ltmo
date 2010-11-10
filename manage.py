39dd255e5b777c765f778f332511d1f20e7811e7
Matias Iturburu <maturburu@gmail.com>
2010-11-09 21:06:30 -0300

    updated the management script as it's not a egg anymore

--- manage.py
+++ manage.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
+
 from django.core.management import execute_manager
 from ltmo import settings
 
 def manage():
     return execute_manager(settings)
+    
+if __name__ == '__main__':
+    manage()
