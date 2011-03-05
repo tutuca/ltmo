f2092d72611392ee5ca72dfd7a6e6420d9646fa8
Matias Iturburu <maturburu@gmail.com>
2011-03-05 03:37:14 -0300

    Mejor feedback para los campos requeridos. Hice requerido el campo tags. Hago que aparezca enfocado el campo description por defecto

--- forms.py
+++ forms.py
@@ -1,7 +1,11 @@
-from ltmo.models import Leak
 from django import forms
+from tagging.forms import TagField
+
+from ltmo.models import Leak
+
 
 class LeakForm(forms.ModelForm):
+    tags = TagField(required=True)    
     class Meta:
         model = Leak
         exclude = ['rendered',]
