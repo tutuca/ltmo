581ad1b4353fadfff9fcdeb703ffea8a0b170f56
Matías Iturburu <maturburu@gmail.com>
2012-11-23 01:22:42 -0300

    faltaba del comit anterior

---
+++ ltmo/manage.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def do_manage():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ltmo.settings")
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+    
+if __name__ == "__main__":
+    do_manage()
+
